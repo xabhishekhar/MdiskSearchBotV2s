37e58d22a75209e84f69b271733985869b456a24
xabhishekhar <72898173+xabhishekhar@users.noreply.github.com>
2023-07-08 00:06:59 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/xabhishekhar/MdiskSearchBotV2.git /MdiskSearchBotV2
-cd /MdiskSearchBotV2
+git clone https://github.com/xabhishekhar/MdiskSearchBotV2s.git /MdiskSearchBotV2s
+cd /MdiskSearchBotV2s
 pip3 install -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
