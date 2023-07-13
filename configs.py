462525bca65ce058f3a142727a1fab09362b6ee0
xabhishekhar <72898173+xabhishekhar@users.noreply.github.com>
2023-07-13 09:51:20 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -12,8 +12,8 @@ class Config(object):
     USER_SESSION_STRING = os.getenv("USER_SESSION_STRING", "")
     CHANNEL_ID = int(os.getenv("CHANNEL_ID", "-1001904406207")) 
     BOT_USERNAME = os.getenv("BOT_USERNAME", "Atozflixbot")
-    BOT_OWNER = int(os.getenv("BOT_OWNER", "5305304520"))
-#    OWNER_USERNAME = os.getenv("OWNER_USERNAME", "misssunknown")
+    BOT_OWNER = int(os.getenv("BOT_OWNER", "5790359537"))
+#    OWNER_USERNAME = os.getenv("OWNER_USERNAME", "itsunknownbroo")
     BACKUP_CHANNEL = os.getenv("BACKUP_CHANNEL", "directfilemovieflix")
 #    GROUP_USERNAME = os.getenv("GROUP_USERNAME")
     START_MSG = os.getenv("START_MSG", """**Hᴇʏ {}, 
